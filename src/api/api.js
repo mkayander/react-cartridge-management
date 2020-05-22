35bb39473e100a272d0e3633afaf4882e0fa553b
Crass Air <CrassAir92@gmail.com>
2020-05-22 10:22:10 +0300

    edit api and build

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://ps-bykrc.dellin.local/api/",
-    baseURL: "http://it-vlshv.dellin.local/api/",
+    baseURL: "http://" + window.location.host + "/api/",
     // baseURL: "http://127.0.0.1:8000/api/",
     responseType: "json",
 });
