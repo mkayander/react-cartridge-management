c1363d7326c47ca43a4cb3d5a454b1ce93381525
MaxKayan <maxim.kayander1@gmail.com>
2020-05-26 15:18:42 +0300

    Commented out unused import Chat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import {Grid} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
-import {withSnackbar} from "notistack";
+import { withSnackbar } from "notistack";
 
 import {
     CartridesTable,
     SuppliesEditable,
     OrdersTable,
-    Chat,
+    // Chat,
 } from "../components";
-import {CommonApi} from "../api/CommonApi";
+import { CommonApi } from "../api/CommonApi";
 import fetchAll from "../api";
 
-import {isMobile} from 'react-device-detect';
-import {Redirect} from "react-router-dom";
+import { isMobile } from "react-device-detect";
+import { Redirect } from "react-router-dom";
 
 class Home extends Component {
     state = {
@@ -27,10 +27,10 @@ class Home extends Component {
 
     displayActions = {
         success: async (msg) => {
-            this.props.enqueueSnackbar(msg, {variant: "success"});
+            this.props.enqueueSnackbar(msg, { variant: "success" });
         },
         error: async (msg) => {
-            this.props.enqueueSnackbar(`${msg}`, {variant: "error"});
+            this.props.enqueueSnackbar(`${msg}`, { variant: "error" });
             console.log("dispError:", msg);
         },
         msg: async (msg) => {
@@ -39,7 +39,7 @@ class Home extends Component {
     };
 
     handleRefresh = async () => {
-        this.setState({loading: true});
+        this.setState({ loading: true });
         console.log("handleRefresh");
         fetchAll()
             .catch((error) => {
@@ -62,7 +62,7 @@ class Home extends Component {
                     });
                 }
             })
-            .finally(() => this.setState({loading: false}));
+            .finally(() => this.setState({ loading: false }));
     };
 
     supplyApi = new CommonApi(
@@ -84,7 +84,7 @@ class Home extends Component {
         {
             refreshAll: this.handleRefresh,
             // setState: (value) => this.setState({ suppliesData: value }),
-            setLoading: (bool) => this.setState({loading: bool}),
+            setLoading: (bool) => this.setState({ loading: bool }),
             success: this.displayActions.success,
             error: this.displayActions.error,
             msg: this.displayActions.msg,
@@ -109,7 +109,7 @@ class Home extends Component {
         },
         {
             refreshAll: this.handleRefresh,
-            setLoading: (bool) => this.setState({loading: bool}),
+            setLoading: (bool) => this.setState({ loading: bool }),
             success: this.displayActions.success,
             error: this.displayActions.error,
             msg: this.displayActions.msg,
@@ -130,7 +130,7 @@ class Home extends Component {
         } = this.state;
 
         if (isMobile && this.props.location.backLink === undefined) {
-            return <Redirect to='/mobile'/>
+            return <Redirect to="/mobile" />;
         } else {
             return (
                 <Grid container spacing={3}>
@@ -139,7 +139,7 @@ class Home extends Component {
                     <CircularProgress disableShrink />
                 </Grid> */}
                     <Grid key="cartridges" xs={12} lg={4} item>
-                        <CartridesTable cartridges={cartridgesData}/>
+                        <CartridesTable cartridges={cartridgesData} />
                     </Grid>
                     <Grid key="supplies" xs={12} lg={8} item>
                         <SuppliesEditable
